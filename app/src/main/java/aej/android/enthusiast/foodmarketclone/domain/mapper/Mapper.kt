90d4efffc7677dbc62b46cd333acaf8ee938ddd5
Yana <44026935+yanajulias@users.noreply.github.com>
2023-09-24 12:33:33 +0700

    Create mapper

---
+++ app/src/main/java/aej/android/enthusiast/foodmarketclone/domain/mapper/Mapper.kt
@@ -0,0 +1,16 @@
+package aej.android.enthusiast.foodmarketclone.domain.mapper
+
+import aej.android.enthusiast.foodmarketclone.domain.model.User
+import aej.android.enthusiast.foodmarketclone.frameworks.http.model.response.AuthDto
+
+fun AuthDto.UserDto?.toUser(): User {
+    return User(
+        address = this?.address.orEmpty(),
+        city = this?.city.orEmpty(),
+        email = this?.email.orEmpty(),
+        houseNumber = this?.houseNumber.orEmpty(),
+        id = this?.id ?: 0,
+        name = this?.name.orEmpty(),
+        phoneNumber = this?.phoneNumber.orEmpty()
+    )
+}
