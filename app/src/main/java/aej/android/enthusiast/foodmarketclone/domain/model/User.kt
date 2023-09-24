4488f7565acca6cbba17d62fafba7b57ecc232d8
Yana <44026935+yanajulias@users.noreply.github.com>
2023-09-24 12:29:43 +0700

    Create user model

---
+++ app/src/main/java/aej/android/enthusiast/foodmarketclone/domain/model/User.kt
@@ -0,0 +1,11 @@
+package aej.android.enthusiast.foodmarketclone.domain.model
+
+data class User(
+    val address: String,
+    val city: String,
+    val email: String,
+    val houseNumber: String,
+    val id: Int,
+    val name: String,
+    val phoneNumber: String
+)
