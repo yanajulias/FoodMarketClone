5ff2213e39776be1562884569482939fe3266a62
Yana <44026935+yanajulias@users.noreply.github.com>
2023-09-27 03:14:51 +0700

    Create http module

---
+++ app/src/main/java/aej/android/enthusiast/foodmarketclone/frameworks/http/di/HttpModule.kt
@@ -0,0 +1,61 @@
+package aej.android.enthusiast.foodmarketclone.frameworks.http.di
+
+import aej.android.enthusiast.foodmarketclone.frameworks.http.model.FoodMarketApi
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HttpModule {
+    @Singleton
+    @Provides
+    fun provideGson(): Gson = Gson()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): FoodMarketApi {
+        return Retrofit.Builder()
+            .baseUrl("https://foodmarket-api.aryaaditiya.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(FoodMarketApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkhttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        authInterceptor: Interceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(authInterceptor)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+        return loggingInterceptor
+    }
+
+}
