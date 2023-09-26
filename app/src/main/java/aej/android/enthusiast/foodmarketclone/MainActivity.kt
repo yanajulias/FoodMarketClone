d201281967c212f2cb783078958868c6906c6f4a
Yana <44026935+yanajulias@users.noreply.github.com>
2023-09-27 03:15:43 +0700

    Update MainActivity

--- app/src/main/java/aej/android/enthusiast/foodmarketclone/MainActivity.kt
+++ app/src/main/java/aej/android/enthusiast/foodmarketclone/MainActivity.kt
@@ -6,11 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import aej.android.enthusiast.foodmarketclone.ui.theme.FoodMarketCloneTheme
+import aej.android.enthusiast.foodmarketclone.theme.FoodMarketCloneTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +21,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Yana")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     FoodMarketCloneTheme {
-        Greeting("Android")
+
     }
 }
